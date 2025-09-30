a400d6d644d3bb720b28eeb30f208e3403354707
PCI Mapper Developer <user@example.com>
2025-09-29 20:41:06 -0600

    Remove non-existent @sveltejs/adapter-firebase, use adapter-auto instead - Works with Firebase Hosting deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,9 @@
-import adapter from '@sveltejs/adapter-firebase';
+import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter({
-      firebaseProject: 'mapping-772cf',
-      functions: {
-        source: 'functions'
-      }
-    }),
+    adapter: adapter(),
     alias: {
       $lib: 'src/lib',
       $app: 'src/app'
