80f6fd99852a54b06fcebc33af522f7a48168d1e
PCI Mapper Developer <user@example.com>
2025-10-31 13:46:54 -0600

    revert: restore VGA output with vga=normal - User needs video output for display - Keep text-only installer (DEBIAN_FRONTEND=text, text parameter) - Restore vga=normal with nomodeset nofb for minimal framebuffer

--- gce-backend/routes/epc-deployment.js
+++ gce-backend/routes/epc-deployment.js
@@ -252,10 +252,9 @@ set default=auto
 insmod gzio
 
 menuentry "Debian 12 Netboot (Automated EPC Install)" --id auto {
-  # ⚠️  TEXT-ONLY INSTALL: No VGA/framebuffer - pure text console only
-  # Removed all vga/video parameters - framebuffer completely disabled
-  # nomodeset nofb = disable framebuffer, console only to serial/tty
-  linux /debian/vmlinuz auto=true priority=critical preseed/url=http://\${GCE_PUBLIC_IP}/downloads/netboot/\${PRESEED_NAME} preseed/file=/cdrom/preseed.cfg preseed/interactive=false DEBIAN_FRONTEND=text DEBCONF_NONINTERACTIVE_SEEN=true net.ifnames=0 biosdevname=0 nomodeset nofb console=ttyS0,115200n8 console=tty1 text ---
+  # ⚠️  TEXT-ONLY INSTALL: 'text' + 'DEBIAN_FRONTEND=text' force text mode, no GUI/TUI installer
+  # vga=normal enables VGA output for display, nomodeset nofb keeps framebuffer minimal
+  linux /debian/vmlinuz auto=true priority=critical preseed/url=http://\${GCE_PUBLIC_IP}/downloads/netboot/\${PRESEED_NAME} preseed/file=/cdrom/preseed.cfg preseed/interactive=false DEBIAN_FRONTEND=text DEBCONF_NONINTERACTIVE_SEEN=true net.ifnames=0 biosdevname=0 vga=normal nomodeset nofb console=ttyS0,115200n8 console=tty1 text ---
   initrd /debian/initrd.gz
 }
 
@@ -519,10 +518,9 @@ set default=auto
 insmod gzio
 
 menuentry "Debian 12 Netboot (Automated EPC Install)" --id auto {
-  # ⚠️  TEXT-ONLY INSTALL: No VGA/framebuffer - pure text console only
-  # Removed all vga/video parameters - framebuffer completely disabled
-  # nomodeset nofb = disable framebuffer, console only to serial/tty
-  linux /debian/vmlinuz auto=true priority=critical preseed/url=http://\${GCE_PUBLIC_IP}/downloads/netboot/\${PRESEED_NAME} preseed/file=/cdrom/preseed.cfg preseed/interactive=false DEBIAN_FRONTEND=text DEBCONF_NONINTERACTIVE_SEEN=true net.ifnames=0 biosdevname=0 nomodeset nofb console=ttyS0,115200n8 console=tty1 text ---
+  # ⚠️  TEXT-ONLY INSTALL: 'text' + 'DEBIAN_FRONTEND=text' force text mode, no GUI/TUI installer
+  # vga=normal enables VGA output for display, nomodeset nofb keeps framebuffer minimal
+  linux /debian/vmlinuz auto=true priority=critical preseed/url=http://\${GCE_PUBLIC_IP}/downloads/netboot/\${PRESEED_NAME} preseed/file=/cdrom/preseed.cfg preseed/interactive=false DEBIAN_FRONTEND=text DEBCONF_NONINTERACTIVE_SEEN=true net.ifnames=0 biosdevname=0 vga=normal nomodeset nofb console=ttyS0,115200n8 console=tty1 text ---
   initrd /debian/initrd.gz
 }
 
