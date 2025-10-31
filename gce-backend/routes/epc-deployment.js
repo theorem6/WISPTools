ba75f969b7d5b9397ebf60899d8b396f340497eb
PCI Mapper Developer <user@example.com>
2025-10-31 14:46:54 -0600

    fix: remove set gfxpayload and explicitly disable all framebuffer drivers - Remove set gfxpayload=text (can enable framebuffer) - Add video=vesafb:off video=efifb:off to disable framebuffer drivers - Keep nomodeset nofb for complete framebuffer disable

--- gce-backend/routes/epc-deployment.js
+++ gce-backend/routes/epc-deployment.js
@@ -253,10 +253,10 @@ set default=auto
 insmod gzio
 
 menuentry "Debian 12 Netboot (Automated EPC Install)" --id auto {
-  # ⚠️  TEXT-ONLY INSTALL: 'text' + 'DEBIAN_FRONTEND=text' force text mode, no GUI/TUI installer
-  # set gfxpayload=text - Use text mode for GRUB (replaces deprecated vga=normal)
-  set gfxpayload=text
-  linux /debian/vmlinuz auto=true priority=critical preseed/url=http://\${GCE_PUBLIC_IP}/downloads/netboot/\${PRESEED_NAME} preseed/file=/cdrom/preseed.cfg preseed/interactive=false DEBIAN_FRONTEND=text DEBCONF_NONINTERACTIVE_SEEN=true net.ifnames=0 biosdevname=0 nomodeset nofb console=ttyS0,115200n8 console=tty1 text ---
+  # ⚠️  TEXT-ONLY INSTALL: Disable framebuffer completely
+  # Remove set gfxpayload=text - it can enable framebuffer
+  # Use kernel parameters only: nomodeset nofb text
+  linux /debian/vmlinuz auto=true priority=critical preseed/url=http://\${GCE_PUBLIC_IP}/downloads/netboot/\${PRESEED_NAME} preseed/file=/cdrom/preseed.cfg preseed/interactive=false DEBIAN_FRONTEND=text DEBCONF_NONINTERACTIVE_SEEN=true net.ifnames=0 biosdevname=0 nomodeset nofb video=vesafb:off video=efifb:off text console=ttyS0,115200n8 console=tty1 ---
   initrd /debian/initrd.gz
 }
 
@@ -521,10 +521,10 @@ set default=auto
 insmod gzio
 
 menuentry "Debian 12 Netboot (Automated EPC Install)" --id auto {
-  # ⚠️  TEXT-ONLY INSTALL: 'text' + 'DEBIAN_FRONTEND=text' force text mode, no GUI/TUI installer
-  # set gfxpayload=text - Use text mode for GRUB (replaces deprecated vga=normal)
-  set gfxpayload=text
-  linux /debian/vmlinuz auto=true priority=critical preseed/url=http://\${GCE_PUBLIC_IP}/downloads/netboot/\${PRESEED_NAME} preseed/file=/cdrom/preseed.cfg preseed/interactive=false DEBIAN_FRONTEND=text DEBCONF_NONINTERACTIVE_SEEN=true net.ifnames=0 biosdevname=0 nomodeset nofb console=ttyS0,115200n8 console=tty1 text ---
+  # ⚠️  TEXT-ONLY INSTALL: Disable framebuffer completely
+  # Remove set gfxpayload=text - it can enable framebuffer
+  # Use kernel parameters only: nomodeset nofb text
+  linux /debian/vmlinuz auto=true priority=critical preseed/url=http://\${GCE_PUBLIC_IP}/downloads/netboot/\${PRESEED_NAME} preseed/file=/cdrom/preseed.cfg preseed/interactive=false DEBIAN_FRONTEND=text DEBCONF_NONINTERACTIVE_SEEN=true net.ifnames=0 biosdevname=0 nomodeset nofb video=vesafb:off video=efifb:off text console=ttyS0,115200n8 console=tty1 ---
   initrd /debian/initrd.gz
 }
 
