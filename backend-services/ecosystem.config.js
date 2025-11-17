c7da2f3886cf6cb4a57450f58f3cd6cb6661a8ba
PCI Mapper Developer <user@example.com>
2025-11-16 20:50:42 -0600

    Update ArcGIS API key to #2 (AT2_12sjSDHZ)

--- backend-services/ecosystem.config.js
+++ backend-services/ecosystem.config.js
@@ -18,7 +18,7 @@ module.exports = {
         HOST: '0.0.0.0',
         FIREBASE_PROJECT_ID: 'wisptools-production',
         FIREBASE_SERVICE_ACCOUNT_KEY: '/opt/lte-pci-mapper/backend-services/wisptools-production-firebase-adminsdk.json',
-        ARCGIS_API_KEY: 'AAPTxy8BH1VEsoebNVZXo8HurGbnlTRSD_LpCIzNN_GYtkaaVGZtaA5vQ2USVJ_79QPe7yOvuh8tFFM0WMguSFCMJ7IG8r9rfuM6fzDUDviu7eAvqgqA9nu1J3mT3jbv_rUPKU2dgp9H8qtEZDOjJ2G_nNM9XWuipReP9xbzFvg8Bo24782l8wWU8RMjLGcxEw_5aKtJZ-XMGb3PlBMj5wTBiF84JeWXIppu_wJ504qzB7A.AT1_12sjSDHZ'
+        ARCGIS_API_KEY: 'AAPT85fOqywZsicJupSmVSCGrjWNNjURUpnE--wnh6GZUrdoxPtDwnuleQ7w-D3IjAfeg3sWvEIvfQitmq894hp5hUN7nAsjLja-hlpzZZekEgdf3besreVsmrBJqy9c2XCRMc5EnBZyP60U1Lhf6E4ZrPWlOoxUWLIe8cCoNPI1Zh9VL1o_1wRQglmdGLTqx9vbHon7Pa_hZyTKQxvVC2stN7sZKy4quJ6kiAtW1QpqwOo.AT2_12sjSDHZ'
       },
       error_file: '/home/david/.pm2/logs/main-api-error.log',
       out_file: '/home/david/.pm2/logs/main-api-out.log',
@@ -43,7 +43,7 @@ module.exports = {
         HOST: '0.0.0.0',
         FIREBASE_PROJECT_ID: 'wisptools-production',
         FIREBASE_SERVICE_ACCOUNT_KEY: '/opt/lte-pci-mapper/backend-services/wisptools-production-firebase-adminsdk.json',
-        ARCGIS_API_KEY: 'AAPTxy8BH1VEsoebNVZXo8HurGbnlTRSD_LpCIzNN_GYtkaaVGZtaA5vQ2USVJ_79QPe7yOvuh8tFFM0WMguSFCMJ7IG8r9rfuM6fzDUDviu7eAvqgqA9nu1J3mT3jbv_rUPKU2dgp9H8qtEZDOjJ2G_nNM9XWuipReP9xbzFvg8Bo24782l8wWU8RMjLGcxEw_5aKtJZ-XMGb3PlBMj5wTBiF84JeWXIppu_wJ504qzB7A.AT1_12sjSDHZ'
+        ARCGIS_API_KEY: 'AAPT85fOqywZsicJupSmVSCGrjWNNjURUpnE--wnh6GZUrdoxPtDwnuleQ7w-D3IjAfeg3sWvEIvfQitmq894hp5hUN7nAsjLja-hlpzZZekEgdf3besreVsmrBJqy9c2XCRMc5EnBZyP60U1Lhf6E4ZrPWlOoxUWLIe8cCoNPI1Zh9VL1o_1wRQglmdGLTqx9vbHon7Pa_hZyTKQxvVC2stN7sZKy4quJ6kiAtW1QpqwOo.AT2_12sjSDHZ'
       },
       error_file: '/home/david/.pm2/logs/epc-api-error.log',
       out_file: '/home/david/.pm2/logs/epc-api-out.log',
